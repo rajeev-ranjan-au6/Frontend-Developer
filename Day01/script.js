3c7da23817a621e114e71b64024a66b8305dbde4
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-05-21 03:47:20 +0530

    Add files via upload

---
+++ Day01/script.js
@@ -0,0 +1,168 @@
+/* this is first video 4
+
+var FirstName ="John";
+console.log(FirstName);
+var lastname ="smith";
+
+var age =28;
+
+var age =true;
+console.log(age);
+var job;
+
+job ="Teacher";
+console.log(job);
+
+ */
+
+/* type coercion*/
+
+/*
+var job, isMarried;
+
+isMarried ="False";
+
+console.log(FirstName + 'is a' + age + "year old" + job + "Married" + isMarried);
+
+alert("Alert!!");
+
+var lastname = prompt(FirstName + "What is his last name?");
+console.log(FirstName + lastname);
+
+*/
+
+/* Opertator*/
+/*
+var year, yearjohn, yearmark;
+
+now = 2018;
+age_john = 28;
+age_mark = 33;
+
+
+yearjohn = now - age_john;
+yearmark = now - age_mark;
+
+console.log(yearjohn);
+console.log(yearmark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now * 2);
+console.log(now % 2);
+*/
+
+/* logical operator*/
+
+/*
+var johnOlder= age_john < age_mark;
+console.log(johnOlder);
+*/
+/*
+/* Typeof operator*/
+/*
+console.log(typeof johnOlder);
+console.log(typeof age_mark);
+
+var X;
+console.log(typeof X);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
