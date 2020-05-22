5bb324c734c4cc9ba0eeff3c097a2aed22037ff9
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-05-23 02:38:53 +0530

    Add files via upload

---
+++ Day03/script.js
@@ -0,0 +1,107 @@
+/*
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operation 
+
+var isfullAge = now - yearJohn>=fullAge;
+
+console.log(isfullAge);
+
+//Grouping operation
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark)/2;
+console.log(average);
+
+//multiple assignment
+
+var x,y;
+x = y =(3+5)*4-6;
+console.log(x, y);
+
+//more opertation
+// x = x * 2;
+
+//console.log(x);
+
+x *= 2;
+console.log(x);
+
+x+=10;
+
+console.log(x);
+
+//increament
+
+x= x+1;
+x+=1;
+x++;
+console.log(x);
+
+x--;
+x-=1;
+x= x-1;
+console.log(x);
+
+
+*/
+
+//coding Challanges
+/*
+
+var wgMark = 78; //kg
+var heightMark = 1.62; //mtr
+
+var wgJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = wgMark / (heightMark*heightMark);
+var BMIJohn = wgJohn / (heightJohn*heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
