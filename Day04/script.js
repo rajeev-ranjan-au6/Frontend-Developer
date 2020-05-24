abe2107a72743d0a211d35ab0e2ab51d74e1764e
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-05-24 07:20:39 +0530

    Add files via upload

---
+++ Day04/script.js
@@ -0,0 +1,94 @@
+//If/else statement
+
+/*
+var FirstName = "John";
+var civilstatus = "single";
+
+if(civilstatus == "married"){
+    console.log(FirstName +" " +  "is married");
+}
+    else{
+        console.log(FirstName + " " + "will marry soon");
+    }
+    
+
+     var FirstName = "John";
+     var age = 22;
+
+     if (age < 13){
+         console.log(FirstName + " " + "is a boy!!");
+
+     }else if(age >= 13 && age < 20){
+         console.log(FirstName + " " + " is a teenager!!!");
+
+     }else if(age >=20 && age < 30){
+         console.log(FirstName + " " + "is a young boy:)");
+     }
+     else{
+         console.log(FirstName + " " + "is a man");
+     }
+
+     */
+/*
+     var FirstName = "john";
+     var age = 10;
+
+     //ternary operator
+
+     age >= 18 ? console.log(FirstName + " " + "Drink Beer:") : console.log(FirstName + " " + "drink Juice:");
+     var drink = age >= 18 ? "beer" : "juice";
+     console.log(drink);
+
+     //switch statement
+     
+     var job = "Desiner";
+     switch(job){
+         case "instructor":
+             console.log(FirstName + " " + "teaches children how to code");
+             break;
+     
+        case "Driver":
+            console.log(FirstName + " " + "he drives car in karnataka:");
+            break;
+     
+
+    case "Desiner":
+        console.log(FirstName + " " + "Is a desiner at bollywood:");
+        break;
+     }
+
+
+
+
+
+var FirstName = "John";
+var age = 22;
+switch(true){
+  case age < 13:
+    console.log(FirstName + " " + "is a boy!!");
+    break;
+
+
+case age >= 13 && age < 20 : 
+    console.log(FirstName + " " + " is a teenager!!!");
+    break;
+
+case age >=20 && age < 30:
+    console.log(FirstName + " " + "is a young boy:)");
+default:
+    console.log(FirstName + " " + "is a man");}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+     
