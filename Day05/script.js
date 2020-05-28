af479a51f50a66541b7849cba03c054c46a4d7f3
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-05-28 21:28:26 +0530

    Add files via upload

---
+++ Day05/script.js
@@ -0,0 +1,71 @@
+//truthy and falsy values and euality operator
+/*
+var height;
+height = 23;
+
+if (height || height === 0){
+    console.log("variable is defined:");
+}else { 
+    console.log("Vaiable has NOT been defined:");
+}
+
+//Equality operator
+
+if ( height === '23'){
+    console.log("the == operator does type coercion:::");
+}
+*/
+
+var scorejohn = (189+120+103)/3;
+var scoreMike = (129+94+123)/3;
+var scoremary = (97+134+105)/3;
+
+console.log(scorejohn, scoreMike, scoremary);
+if(scorejohn> scoreMike> scoremary){
+    console.log("mike\s team win " +  scoreMike + "point")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
