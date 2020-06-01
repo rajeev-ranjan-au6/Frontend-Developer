8f4bb7f196a71e62492ff9ce08b9edc7e2dabced
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-06-01 20:45:10 +0530

    Add files via upload

---
+++ Day06/script.js
@@ -0,0 +1,101 @@
+/*
+function calculateAge(birthYear){
+    return 2018-birthYear;
+}
+
+var ageJohn = calculateAge(1996);
+var ageMike = calculateAge(1948);
+var ageJene = calculateAge(1969);
+
+console.log(ageJene, ageMike, ageJene);
+
+
+
+function retirement (year, firstName){
+    var age= calculateAge(year);
+    var retirement= 65-age;
+
+    if(retirement>0){
+        console.log(firstName + 'retires in '+ retirement+'year:');
+    }
+    else{
+        console.log(firstName + "is already retired:");
+    }
+}
+
+retirement(1990, 'John');
+retirement(1948, 'Mark');
+retirement(1969-'Jene');
+*/
+/*Arrays*/
+/*
+var names = ['John', 'Mike', 'Jene'];
+var year = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+//mutate array data
+
+names[1]='Ben';
+names[names.length]='mary';
+console.log(names);
+
+//differnt data types
+
+var John = ['John', 'smith', 1990, 'designer', false];
+John.push('blue');console.log(John);
+
+John.pop();
+John.pop();
+John.shift();
+
+console.log(John);
+console.log(John.indexOf(23));
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
