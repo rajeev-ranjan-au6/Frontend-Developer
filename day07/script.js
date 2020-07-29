8d7cfc50d41f0a9a0ea4f723ddc1fd49c9c564cc
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-07-29 18:53:24 +0530

    Basic of variables

---
+++ day07/script.js
@@ -0,0 +1,73 @@
+/*
+//welcome massage
+
+console.log("hello!!"); //external script
+
+//variables
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'smith';
+
+//datatypes
+
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+console.log(firstName+lastName+"is of age  "+fullAge);
+
+var job;
+console.log(job);  //undefined token
+job = 'teacher';
+console.log(job);
+
+
+
+// type coerction
+
+var firstName = 'john';
+var age = 28;
+console.log(firstName + " " + age); //this is called as time coercion that convert one data into another
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + "is a " + age +"  " + "  year old" + job +". is he married?? " + isMarried);
+
+//variable mutation
+
+age = 'twenty eight';
+job = 'Driver';
+alert(firstName + "is a " + age +"  " + "  year old" + job +". is he married?? " + isMarried);
+
+var lastName = prompt("What is his last Name");
+console.log(firstName + " " +lastName);
+
+
+//operators
+var year = 90;
+//math operator
+
+var yearJohn = 2018-28;
+var yearMark = year-30;
+
+console.log(yearJohn + " " + yearMark);
+console.log(yearMark * yearJohn);
+console.log(yearMark / 20);
+
+//logical operator
+
+var johnOlder = yearJohn < yearMark;
+
+console.log(johnOlder);
+
+//type of operator
+
+console.log(typeof johnOlder);
+var x;
+console.log(typeof x);
+
+*/
